acc82e51a6068c6acfa3d699c9a435b8ea750b70
Lakshanya1210 <92517428+Lakshanya1210@users.noreply.github.com>
2024-04-25 06:27:51 +0530

    Delete user_auth directory

--- user_auth/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///users.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_secret_key')
